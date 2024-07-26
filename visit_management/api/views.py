71cd7139e1f6e8f97a4f6b185b4560cf96c6a53b
mxnoob <noobmx@gmail.com>
2024-07-26 12:59:37 +0300

    add authentication

--- visit_management/api/views.py
+++ visit_management/api/views.py
@@ -6,18 +6,9 @@ from core.models import Worker, Shop, Visit
 from .serializers import VisitSerializer, ShopSerializer
 
 
-def get_worker(request):
-    phone_number = request.headers.get("Authorization", "").replace(
-        "Phone ", ""
-    )
-    return get_object_or_404(Worker, phone_number=phone_number)
-
-
 class ShopListViews(APIView):
     def get(self, request, format=None):
-        worker = get_worker(request)
-
-        shops = worker.shops
+        shops = request.user.shops
         if not shops:
             return Response({"message": "No shops found"}, status=404)
 
@@ -27,7 +18,7 @@ class ShopListViews(APIView):
 
 class VisitViews(APIView):
     def post(self, request, shop_id, format=None):
-        worker = get_worker(request)
+        worker = request.user
         get_object_or_404(Shop, id=shop_id, worker=worker)
         serializer = VisitSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
--- visit_management/visit_management/settings.py
+++ visit_management/visit_management/settings.py
@@ -124,6 +124,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.BasicAuthentication",
+        "api.authentication.PhoneAuthentication",
     ),
 }
